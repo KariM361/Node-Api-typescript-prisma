63b66af1995dabaf39379ee46e85b263fcb78719
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-12-04 15:16:00 +0100

    new

--- h1we080125-typescript-everride/src/services/loginUser.ts
+++ h1we080125-typescript-everride/src/services/loginUser.ts
@@ -1,3 +1,42 @@
+import{ prisma } from '../prisma.js';
+import bcrypt  from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'jwtsecret';
+
 export const loginUser = async (email: string, password: string) => {
 
+    const user = await prisma.user.findUnique({
+        where: { email },
+        select: { id: true,
+             firstname: true, 
+             lastname: true,
+              email: true, 
+              password: true,
+               role: true }
+    });
+
+    if (!user) {
+        throw new Error('user dosent exist');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password); 
+    if (!isPasswordValid) {
+        throw new Error('Wrong password');
+}
+    const token = jwt.sign({
+        id: user.id,
+       fullname: `${user.firstname} ${user.lastname}`,
+       role: user.role
+    },
+    JWT_SECRET,
+    { expiresIn: '1h' }//token udløber efter 1 time
+    );
+
+    const { password: _password,  ...userWithoutPassword } = user;
+
+    return {
+        user: userWithoutPassword,
+        token
+    };
 }
